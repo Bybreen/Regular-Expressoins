eaed852ff7ee2c1ae5aa214633528e28f02df071
Bybreen Samuels <bybreensamuels@googlemail.com>
2019-06-26 16:47:35 +0100

    Specify Only the Lower Number of Matches
    You can specify the lower and upper number of patterns with quantity specifiers using curly brackets. Sometimes you only want to specify the lower number of patterns with no upper limit.
    
    To only specify the lower number of patterns, keep the first number followed by a comma.
    
    For example, to match only the string "hah" with the letter a appearing at least 3 times, your regex would be /ha{3,}h/.
    
    let A4 = "haaaah";
    let A2 = "haah";
    let A100 = "h" + "a".repeat(100) + "h";
    let multipleA = /ha{3,}h/;
    multipleA.test(A4); // Returns true
    multipleA.test(A2); // Returns false
    multipleA.test(A100); // Returns true

---
+++ Specifying-Only-the-Lower-Number-of-Matches.js
@@ -0,0 +1,5 @@
+// Change the regex haRegex to match the word "Hazzah" only when it has four or more letter z's.
+
+let haStr = "Hazzzzah.";
+let haRegex = /haz{4,}ah/i;
+let result haRegex.match(haStr);
